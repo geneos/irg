577bfb2d447ed50a98a5f494f1de4a30f82ab8b3
Pablo Velaquez <pablocho88@gmail.com>
2017-11-17 10:48:31 -0300

    facebook integration

--- sites/all/themes/theme1032/templates/html.tpl.php
+++ sites/all/themes/theme1032/templates/html.tpl.php
@@ -69,6 +69,17 @@ print $doctype; ?>
 	<link rel="stylesheet" href="<?php echo base_path().path_to_theme() ?>/css/skeleton-mobile.css" media="screen" id="skeleton-mobile">
 </head>
 <body id="body" class="<?php print $classes; ?>" <?php print $attributes;?>>
+	<div id="fb-root"></div>
+	
+	<script>(function(d, s, id) {
+	  var js, fjs = d.getElementsByTagName(s)[0];
+	  if (d.getElementById(id)) return;
+	  js = d.createElement(s); js.id = id;
+	  js.src = 'https://connect.facebook.net/es_LA/sdk.js#xfbml=1&version=v2.11&appId=1935830846678961';
+	  fjs.parentNode.insertBefore(js, fjs);
+	}(document, 'script', 'facebook-jssdk'));
+	</script>
+
 	<?php print $page_top;
 	print $page;
 	print $page_bottom; ?>
